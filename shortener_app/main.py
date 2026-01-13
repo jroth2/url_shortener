2a2f26351e831eff6590b86d9adb78aa95c44985
joe <jroth213@gmail.com>
2026-01-13 15:26:58 -0600

    adds basic logic to main.py to handle posts

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from shortener-app!")
+# shortener_app/main.py
 
+import validators
+from fastapi import FastAPI, HTTPException
+from . import schemas
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return "welcome to the world tour"
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400,detail=message)
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your URL is not valid")
+    return f"TODO: Create database entry for {url.target_url}"
