f5752bdeab0e9c7598913a661f5e5a03016301da
joe <jroth213@gmail.com>
2026-01-13 10:31:49 -0600

    formatting

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    env_name: str = 'Local'
+    env_name: str = "Local"
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./shortener.db"
 
